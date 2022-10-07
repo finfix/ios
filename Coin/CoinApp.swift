2a1018cd143c1ce16aa754825a80439de24e957c
Bonavi <iliaivanov27@gmail.com>
2022-10-07 15:36:59 +0700

    Initial Commit

---
+++ Coin/CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinApp.swift
+//  Coin
+//
+//  Created by Илья on 07.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
