aec930555d946c4b305cb9c982879ad47aa6fa4b
Bonavi <iliaivanov27@gmail.com>
2022-10-07 15:47:36 +0700

    Список транзакций
    - Модель транзакций
    - Вызов апи
    - Отображение

--- Coin/ContentView.swift
+++ Coin/ContentView.swift
@@ -8,19 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    /// Добавляем Network в качестве EnvironmentObject
+    @EnvironmentObject var network: Network
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        /// В Body создаем ScrollView, и onAppear мы назовем нашу функцию getUsers:
+        Text("All transactions")
+            .font(.title).bold()
+        ScrollView {
+            /// Выполняем итерацию по network.users. Мы отобразим идентификатор каждого пользователя, имя, адрес электронной почты и телефон.
+            VStack(alignment: .leading) {
+                ForEach(network.transactions) { transaction in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            // Text("\(transaction.dateTransaction)")
+                            Text("\(transaction.accountFromID) -> \(transaction.accountToID)")
+                                .font(.footnote)
+                            
+                            if transaction.amountTo == transaction.amountFrom {
+                                Text(String(format: "%.2f", transaction.amountTo))
+                                    .font(.footnote)
+                            } else {
+                                Text(String(format: "%.2f", transaction.amountFrom) + " -> " + String(format: "%.2f", transaction.amountTo))
+                                    .font(.footnote)
+                            }
+                        }
+                        Spacer()
+                        if let note = transaction.note {
+                            Text(note)
+                                .font(.footnote)
+                        }
+                    }
+                    .frame(width: 300, alignment: .leading)
+                    .padding()
+                    .background(Color(#colorLiteral(red: 0.6667672396, green: 0.7527905703, blue: 1, alpha: 0.2662717301)))
+                    .cornerRadius(20)
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            network.getTransaction()
+        }
     }
 }
 
+/// Чтобы предварительный просмотр работал, не забудьте добавить environmentObject в предварительный просмотр ContentView, так как предварительный просмотр отличается от приложения:
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(Network())
     }
 }
---
+++ Coin/Transaction.swift
@@ -0,0 +1,38 @@
+//
+//  Transaction.swift
+//  Coin
+//
+//  Created by Илья on 07.10.2022.
+//
+
+import Foundation
+
+/// Эта модель соответствует идентифицируемым и декодируемым протоколам. Идентифицируемый означает, что каждый элемент имеет уникальный идентификатор. Декодируемый означает, что его можно декодировать - например, мы можем преобразовать объект JSON в эту модель данных.
+
+struct Transaction: Identifiable, Decodable {
+    var accountFromID: Int
+    var accountToID: Int
+    var accounting: Bool
+    var amountFrom: Double
+    var amountTo: Double
+    // var dateTransaction: Date
+    var id: Int
+    var isExecuted: Bool
+    var note: String?
+    var typeSignatura: String
+    var tagName: [Tag]?
+
+    struct Tag: Decodable {
+        var id: Int
+        var tagID: Int
+        var transactionID: Int
+
+    }
+}
+
+struct ModelError: Decodable {
+    var humanTextError: String
+    var developerTextError: String
+    var context: String?
+}
+
