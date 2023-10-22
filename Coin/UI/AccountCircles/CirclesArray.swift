b7427e9a5451146accaa70cd83a8ca387c908007
Bonavi <iliaivanov27@gmail.com>
2023-10-22 21:16:00 +0700

    extract plus account view

--- Coin/UI/AccountCircles/CirclesArray.swift
+++ Coin/UI/AccountCircles/CirclesArray.swift
@@ -12,32 +12,12 @@ struct CirclesArray: View {
     var accounts: [Account]
     
     var accountsType: AccountType
-    @State var isOpenCreate = false
     
     var body: some View {
-        Group {
-            ForEach(accounts.filter { ($0.type == accountsType) && $0.visible }) { account in
-                AccountCircleItem(account: account)
-            }
-            
-            VStack {
-                ZStack {
-                    Circle()
-                        .stroke(.gray, lineWidth: 2)
-                        .foregroundColor(.clear)
-                        .frame(width: 30)
-                    Image(systemName: "plus")
-                        .foregroundColor(.gray)
-                }
-                .foregroundColor(.primary)
-                .onTapGesture {
-                    isOpenCreate = true
-                }
-            }
-            .navigationDestination(isPresented: $isOpenCreate) {
-                CreateAccount(isOpeningFrame: $isOpenCreate, accountType: accountsType)
-            }
+        ForEach(accounts.filter { ($0.type == accountsType) && $0.visible }) {
+            AccountCircleItem(account: $0)
         }
+        PlusNewAccount(accountType: accountsType)
     }
 }
 
