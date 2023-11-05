b09a915b39845ba4ceec5b750c444e9bc503ab2a
Bonavi <iliaivanov27@gmail.com>
2023-11-05 23:00:36 +0700

    rm accountChildren and CirclesArray Views

--- Coin/UI/AccountCircles/AccountCirclesList.swift
+++ Coin/UI/AccountCircles/AccountCirclesList.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 import SwiftData
 
-struct AccountCircleList: View {
-        
-    @Query var currencies: [Currency]
+struct AccountCirclesListSubView: View {
+    
     @Query var accounts: [Account]
-    @Query var accountGroups: [AccountGroup]
+    @Query var currencies: [Currency]
     @State var path = NavigationPath()
     
     var groupedAccounts: [Account] {
@@ -38,36 +37,45 @@ struct AccountCircleList: View {
             VStack(spacing: 5) {
                 VStack(spacing: 0) {
                     Header()
-                    if accountGroups.count > 1 {
-                        AccountsGroupSelector()
-                    }
+                    AccountsGroupSelector()
                 }
-                ScrollView(.horizontal) {
-                    HStack(spacing: horizontalSpacing) {
-                        CirclesArray(accounts: groupedAccounts, accountsType: .earnings)
+                Group {
+                    ScrollView(.horizontal) {
+                        HStack(spacing: horizontalSpacing) {
+                            ForEach(groupedAccounts.filter { $0.type == .earnings }) { account in
+                                AccountCircleItem(account, path: $path)
+                            }
+                            PlusNewAccount(accountType: .earnings)
+                        }
                     }
-                }
-                
-                Divider()
-                
-                ScrollView(.horizontal) {
-                    HStack(spacing: horizontalSpacing) {
-                        CirclesArray(accounts: groupedAccounts, accountsType: .regular)
+                    
+                    Divider()
+                    
+                    ScrollView(.horizontal) {
+                        HStack(spacing: horizontalSpacing) {
+                            ForEach(groupedAccounts.filter { $0.type == .regular }) { account in
+                                AccountCircleItem(account, path: $path)
+                            }
+                            PlusNewAccount(accountType: .regular)
+                        }
                     }
-                }
-                
-                Divider()
-                
-                ScrollView(.horizontal) {
-                    LazyHGrid(rows: rows, alignment: .top, spacing: horizontalSpacing) {
-                        CirclesArray(accounts: groupedAccounts, accountsType: .expense)
+                    
+                    Divider()
+                    
+                    ScrollView(.horizontal) {
+                        LazyHGrid(rows: rows, alignment: .top, spacing: horizontalSpacing) {
+                            ForEach(groupedAccounts.filter { $0.type == .expense }) { account in
+                                AccountCircleItem(account, path: $path)
+                            }
+                            PlusNewAccount(accountType: .expense)
+                        }
                     }
+                    .frame(maxHeight: .infinity)
                 }
-                .frame(maxHeight: .infinity)
-                Spacer()
                 .navigationDestination(for: Account.self) { UpdateAccount(account: $0) }
                 .navigationDestination(for: AccountType.self) { CreateAccount(accountType: $0) }
             }
+            Spacer()
         }
     }
 }
