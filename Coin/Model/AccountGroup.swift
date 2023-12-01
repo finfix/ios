ef45af45e788fdf553311721d9947db0996af7b9
Bonavi <iliaivanov27@gmail.com>
2023-12-02 00:06:58 +0700

    add relationship for accountGroup

--- Coin/Model/AccountGroup.swift
+++ Coin/Model/AccountGroup.swift
@@ -13,20 +13,24 @@ import SwiftData
     @Attribute(.unique) var id: UInt32
     var name: String
     var currency: Currency?
+    @Relationship(deleteRule: .nullify, inverse: \Account.accountGroup) var accounts: [Account]
         
     init(
         id: UInt32 = 0,
         name: String = "",
-        currency: Currency? = nil
+        currency: Currency? = nil,
+        accounts: [Account] = []
     ) {
         self.id = id
         self.name = name
         self.currency = currency
+        self.accounts = accounts
     }
     
     init(_ res: GetAccountGroupsRes, currenciesMap: [String: Currency]) {
         self.id = res.id
         self.name = res.name
+        self.accounts = []
         self.currency = currenciesMap[res.currency]
     }
 }
